46a7a7cdca2794cf9f707993d2c53facf36afca9
Akash Dahad <44143396+akashdahad@users.noreply.github.com>
2023-08-31 15:20:31 +0530

    Pipeline to Server

--- app/sm.py
+++ app/sm.py
@@ -3,9 +3,16 @@ from langchain.vectorstores import Chroma
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
+from langchain.llms import LlamaCpp
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+n_gpu_layers = 32  # Metal set to 1 is enough.
+n_batch = 512  # Should be between 1 and n_ctx, consider the amount of RAM of your Apple Silicon Chip.
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
 # Load Model
-llm = LlamaCpp(model="./models/llama-2-7b-chat.ggmlv3.q2_K.bin", n_gpu_layers=n_gpu_layers, n_batch=n_batch, n_ctx=2048, f16_kv=True, callback_manager=callback_manager, verbose=False)
+llm = LlamaCpp(model_path="./models/llama-2-7b-chat.ggmlv3.q2_K.bin", n_gpu_layers=n_gpu_layers, n_batch=n_batch, n_ctx=2048, f16_kv=True, callback_manager=callback_manager, verbose=False)
 
 # Embedding Model Details
 embed_model_id = 'sentence-transformers/all-MiniLM-L6-v2'
